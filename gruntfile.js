ed62080a7a5e63df93a903cddcd6b6dd0248f655
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-08-09 19:17:32 -0700

    Add webpack & grunt configuration for build

---
+++ gruntfile.js
@@ -0,0 +1,98 @@
+'use strict'
+
+var _ = require('lodash')
+var webpack = require('webpack')
+
+var mergeWebpackConfig = function (config) {
+  // Load webpackConfig only when using `grunt:webpack`
+  // load of grunt tasks is faster
+  var webpackConfig = require('./webpack.config')
+  return _.merge({}, webpackConfig, config, function (a, b) {
+    if (_.isArray(a)) {
+      return a.concat(b)
+    }
+  })
+}
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      min: {
+        files: {
+          'dist/react-datepicker.css': 'src/stylesheets/datepicker.scss'
+        },
+        options: {
+          sourcemap: 'none',
+          style: 'expanded'
+        }
+      },
+      unmin: {
+        files: {
+          'dist/react-datepicker.min.css': 'src/stylesheets/datepicker.scss'
+        },
+        options: {
+          sourcemap: 'none',
+          style: 'compressed'
+        }
+      }
+    },
+
+    watch: {
+      eslint: {
+        files: ['{src,test,docs-site/src}/**/*.{js,jsx}', '*.js'],
+        tasks: ['eslint']
+      },
+
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      },
+
+      webpack: {
+        files: ['src/**/*.js', 'src/**/*.jsx'],
+        tasks: ['webpack']
+      }
+    },
+
+    // standalone build for ./dist
+    webpack: {
+      unmin: mergeWebpackConfig({
+        output: {
+          filename: 'react-datepicker.js'
+        }
+      }),
+      min: mergeWebpackConfig({
+        output: {
+          filename: 'react-datepicker.min.js'
+        },
+        plugins: [
+          new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+              warnings: false
+            }
+          })
+        ]
+      }),
+    },
+
+    // source build for ./lib
+    babel: {
+      lib: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**/*.js', '**/*.jsx'],
+          dest: 'lib/',
+          ext: '.js'
+        }]
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-babel')
+  grunt.loadNpmTasks('grunt-webpack')
+
+  grunt.registerTask('build', ['babel', 'webpack', 'sass'])
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+
 var webpack = require('webpack')
 
 module.exports = {
   entry: './src/datepicker',
   output: {
     libraryTarget: 'umd',
-    library: 'AutosizeDatePicker',
+    library: 'DatePicker',
     path: './dist/'
   },
   module: {
@@ -18,5 +19,46 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
-  }
+  },
+  externals: [
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    },
+    {
+      'react': {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    },
+    {
+      'moment': {
+        root: 'moment',
+        commonjs2: 'moment',
+        commonjs: 'moment',
+        amd: 'moment'
+      }
+    },
+    {
+      'react-onclickoutside': {
+        root: 'OnClickOutside',
+        commonjs2: 'react-onclickoutside',
+        commonjs: 'react-onclickoutside',
+        amd: 'react-onclickoutside'
+      }
+    }
+  ],
+  node: { Buffer: false },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 }
